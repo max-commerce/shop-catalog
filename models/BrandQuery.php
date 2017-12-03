aa6ea755fa754ad9a21a71f6e6ae77dfbb620f08
web-mayak <web-mayak@mail.ru>
2017-12-03 18:27:04 +1000

    устранена логическая ошибка в запросе на получение списка брендов по категории

--- models/BrandQuery.php
+++ models/BrandQuery.php
@@ -16,12 +16,15 @@ class BrandQuery extends ActiveQuery
 
 	public function byCategory($category)
     {
-        $query = 'SELECT DISTINCT brand_id FROM shop_products WHERE status = 1 AND category_id = :category_id';
-        $params = [':category_id' => $category->id];
+        $query = 'SELECT DISTINCT brand_id FROM shop_products WHERE brand_id <> 0 AND status = 1';
         if ($child_categories = $category->childs) {
+            // TODO сейчас берется список дочерних категорий только 1 уровня, надо брать все
             $child_categories_ids = ArrayHelper::getColumn($child_categories, 'id');
-            $query .= ' OR category_id IN (' . implode(', ', $child_categories_ids) . ')';
+            $query .= ' AND (category_id = :category_id OR category_id IN (' . implode(', ', $child_categories_ids) . '))';
+        } else {
+            $query .= ' AND category_id = :category_id';
         }
+        $params = [':category_id' => $category->id];
         $brand_ids = Yii::$app->db->createCommand($query, $params)->queryColumn();
         $this->andWhere(['id' => $brand_ids]);
         return $this;
